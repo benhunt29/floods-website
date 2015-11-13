f7c319b55fa539293001c432a68d70866e0204f5
benhunt29 <benhunt29@gmail.com>
2015-11-13 17:51:46 -0600

    added moment.js to handle dates, only show upcoming shows

--- client/javascripts/app.js
+++ client/javascripts/app.js
@@ -1,8 +1,16 @@
 var $ = require('jquery');
 var Promise = require('bluebird');
 var Handlebars = require('hbs');
+var moment = require('moment');
 
 $(document).ready(function(){
+
+    $('.shows').hide();
+    $(window).on('hashchange',function(){
+       render(window.location.hash);
+    });
+
+    //$(window).trigger('hashchange');
 
     var
         FB_ID = window.__env.FB_ID,
@@ -10,6 +18,28 @@ $(document).ready(function(){
 
     var shows = getShows();
     //displayShows(shows);
+
+    function render(url){
+        // Get the keyword from the url.
+        var temp = url.split('/')[0];
+
+        // Hide whatever page is currently shown.
+        //$('.main-content .page').removeClass('visible');
+
+        var map = {
+            '': function(){
+                $('.shows').hide();
+            },
+            '#shows': function(){
+               $('.shows').show();
+            }
+        };
+
+        if(map[temp]){
+         map[temp]();
+        }
+
+    }
 
     function getShows(){
         //first request
@@ -24,15 +54,44 @@ $(document).ready(function(){
             .then(function(results){
                 console.log(results);
                 if(results){
-                    displayShows(results.events.data);
+                    var currentShows = getCurrentShows(results.events.data);
+                    displayShows(currentShows);
                 }
             })
             //handle errors
             .catch(function(err){
                 console.log(err);
             });
-
     }
+
+    function getCurrentShows(allShows){
+        var currentShows = [],
+            currentTime = moment().format();
+
+        allShows.forEach(function(item){
+            var showToDisplay = {};
+            if(moment(item.start_time).diff(currentTime) > 0){
+                showToDisplay.title = item.name;
+                showToDisplay.date = moment(item.start_time).format('MMMM DD YYYY');
+                showToDisplay.time = moment(item.start_time).format('hh:mm a');
+                showToDisplay.venue = item.place.name;
+                showToDisplay.city = item.place.location.city;
+                showToDisplay.state = item.place.location.state;
+                currentShows.push(showToDisplay);
+            }
+           // console.log(moment(item.start_time).diff(moment().format()));
+           //console.log(moment(item.start_time).format('MMMM DD YYYY'));
+           console.log(currentShows);
+        });
+
+        return currentShows;
+    }
+
+    var $showsNav = $('#showsNav');
+
+    $showsNav.on('click', function(){
+       window.location.hash = 'shows';
+    });
 
     function displayShows(data){
         var $showsDiv = $('.showDescriptions');
