6ae0f4b02185ce0c1bfba702a3442dc1fd41125d
benhunt29 <benhunt29@gmail.com>
2015-11-13 16:14:15 -0600

    added handlebars and cleaned up CSS

--- client/javascripts/app.js
+++ client/javascripts/app.js
@@ -1,26 +1,48 @@
 var $ = require('jquery');
 var Promise = require('bluebird');
+var Handlebars = require('hbs');
 
 $(document).ready(function(){
 
-    var FB_ID = window.__env.FB_ID;
-    var FB_ACCESS_TOKEN = window.__env.FB_ACCESS_TOKEN;
+    var
+        FB_ID = window.__env.FB_ID,
+        FB_ACCESS_TOKEN = window.__env.FB_ACCESS_TOKEN;
 
-    //first request
-    var fbEvents = $.ajax({
-    url: 'https://graph.facebook.com/' + FB_ID + '/?fields=events&access_token=' + FB_ACCESS_TOKEN,
-    method: 'GET'
-    });
+    var shows = getShows();
+    //displayShows(shows);
 
-    //Bluebird promise
-    Promise.resolve(fbEvents)
-    //runs when fbEvents is fullfilled
-      .then(function(results){
-       console.log(results);
-      })
-    //handle errors
-      .catch(function(err){
-       console.log(err);
-      })
+    function getShows(){
+        //first request
+        var fbEvents = $.ajax({
+            url: 'https://graph.facebook.com/' + FB_ID + '/?fields=events&access_token=' + FB_ACCESS_TOKEN,
+            method: 'GET'
+        });
+
+        //Bluebird promise
+        Promise.resolve(fbEvents)
+            //runs when fbEvents is fullfilled
+            .then(function(results){
+                console.log(results);
+                if(results){
+                    displayShows(results.events.data);
+                }
+            })
+            //handle errors
+            .catch(function(err){
+                console.log(err);
+            });
+
+    }
+
+    function displayShows(data){
+        var $showsDiv = $('.showDescriptions');
+
+        var theTemplateScript = $("#shows-template").html();
+        //Compile the template​
+        var theTemplate = Handlebars.compile (theTemplateScript);
+        $showsDiv.append (theTemplate(data));
+
+    }
+
 
  });
