14c082dbb06149ab67ebf0e183d38b3201d1fedd
benhunt29 <benhunt29@gmail.com>
2015-11-12 18:38:31 -0600

    cleaned up index.html

--- public/javascripts/bundle.js
+++ public/javascripts/bundle.js
@@ -5,22 +5,21 @@ var Promise = require('bluebird');
 $(document).ready(function(){
 
     var FB_ID = window.__env.FB_ID;
-     var FB_ACCESS_TOKEN = window.__env.FB_ACCESS_TOKEN;
+    var FB_ACCESS_TOKEN = window.__env.FB_ACCESS_TOKEN;
 
-  //first request
-  var fbEvents = $.ajax({
-   url: 'https://graph.facebook.com/' + FB_ID + '/?fields=events&access_token=' + FB_ACCESS_TOKEN,
-   method: 'GET'
-  });
+    //first request
+    var fbEvents = $.ajax({
+    url: 'https://graph.facebook.com/' + FB_ID + '/?fields=events&access_token=' + FB_ACCESS_TOKEN,
+    method: 'GET'
+    });
 
-
-  //Bluebird promise
-  Promise.resolve(fbEvents)
-   //runs when fbEvents is fullfilled
+    //Bluebird promise
+    Promise.resolve(fbEvents)
+    //runs when fbEvents is fullfilled
       .then(function(results){
        console.log(results);
       })
-   //handle errors
+    //handle errors
       .catch(function(err){
        console.log(err);
       })
