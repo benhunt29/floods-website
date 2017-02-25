b4e93e7a070a07b8c8fc77a41ab54b039c3717c5
Ben <benhunt29@gmail.com>
2017-02-25 16:23:07 -0600

    don't display show if missing event location

--- client/javascripts/app.js
+++ client/javascripts/app.js
@@ -1,166 +1,166 @@
-var $ = require('jquery');
-var Promise = require('bluebird');
-var Handlebars = require('hbs');
-var moment = require('moment');
+var $ = require('jquery')
+var Promise = require('bluebird')
+var Handlebars = require('hbs')
+var moment = require('moment')
 
-"use strict";
+'use strict'
 
-$(document).ready(function(){
+$(document).ready(function () {
+  var
+    $showsDiv = $('.shows'),
+    $showsNav = $('#showsNav'),
+    $newsNav = $('#newsNav'),
+    $newsDiv = $('.news')
 
-    var
-        $showsDiv = $('.shows'),
-        $showsNav = $('#showsNav'),
-        $newsNav = $('#newsNav'),
-        $newsDiv = $('.news');
-
-    Handlebars.registerHelper('attr', function(name, data) {
-        if(typeof target === 'undefined') {
-            var target = "";
-        }
-
-        var result = ' ' + name + '="' + data +  '" ';
-
-        return new Handlebars.SafeString(result);
-    });
-
-    (function init(){
-        $showsDiv.hide();
-        $newsDiv.hide();
-        getShows();
-        getNews();
-    }());
-
-    function getRandomIntInclusive(min, max) {
-      return Math.floor(Math.random() * (max - min + 1)) + min;
+  Handlebars.registerHelper('attr', function (name, data) {
+    if (typeof target === 'undefined') {
+      var target = ''
     }
 
-    $showsNav.on('click', function(){
-        window.location.hash = 'shows';
-    });
+    var result = ' ' + name + '="' + data + '" '
 
-    $newsNav.on('click', function(){
-       window.location.hash = 'news';
-    });
+    return new Handlebars.SafeString(result)
+  });
 
-    $(window).on('hashchange',function(){
-        render(window.location.hash);
-    });
+  (function init () {
+    $showsDiv.hide()
+    $newsDiv.hide()
+    getShows()
+    getNews()
+  }())
 
-    function render(url){
+  function getRandomIntInclusive (min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min
+  }
+
+  $showsNav.on('click', function () {
+    window.location.hash = 'shows'
+  })
+
+  $newsNav.on('click', function () {
+    window.location.hash = 'news'
+  })
+
+  $(window).on('hashchange', function () {
+    render(window.location.hash)
+  })
+
+  function render (url) {
         // Get the keyword from the url.
-        var temp = url.split('/')[0];
+    var temp = url.split('/')[0]
 
-        var map = {
-            '': function(){
-                $showsDiv.hide();
-                $newsDiv.hide();
-            },
-            '#shows': function(){
-                $newsDiv.hide();
-                $showsDiv.show();
-            },
-            '#news': function(){
-                $showsDiv.hide();
-                $newsDiv.show();
-            }
-        };
-
-        if (map[temp]){
-            map[temp]();
+    var map = {
+      '': function () {
+          $showsDiv.hide()
+          $newsDiv.hide()
+        },
+      '#shows': function () {
+          $newsDiv.hide()
+          $showsDiv.show()
+        },
+      '#news': function () {
+          $showsDiv.hide()
+          $newsDiv.show()
         }
     }
 
-    function getNews(){
-        var news = $.ajax({
-            url: '/api/news',
-            method: 'GET'
-        });
-
-        //Bluebird promise
-        Promise.resolve(news)
-            .then(function(results){
-                if(results){
-                    displayNews(results);
-                }
-            })
-            .then(function(){
-                $(window).trigger('hashchange');
-            })
-            //handle errors
-            .catch(function(err){
-                console.log(err);
-            });
+    if (map[temp]) {
+      map[temp]()
     }
+  }
 
-    function displayNews(data){
-        var $newsDiv = $('.newsDescriptions');
-        data.forEach(function(news){
-           news.backgroundImg = 'background-image: url(\'' + news.imageUrl + '\')';
-        });
+  function getNews () {
+    var news = $.ajax({
+      url: '/api/news',
+      method: 'GET'
+    })
 
-        var newsTemplateScript = $("#news-template").html();
-        //Compile the template​
-        var newsTemplate = Handlebars.compile (newsTemplateScript);
-        $newsDiv.append (newsTemplate(data));
-
-    }
-
-    function getShows(){
-        //ajax request to Facebook API
-        var fbEvents = $.ajax({
-            url: '/api/events',
-            method: 'GET'});
-        //Bluebird promise
-        Promise.resolve(fbEvents)
-            //runs when fbEvents is fullfilled
-            .then(function(results){
-                if (results) {
-                    var allShows = getAllShows(results.data);
-                    displayShows(allShows);
-                }
+        // Bluebird promise
+    Promise.resolve(news)
+            .then(function (results) {
+              if (results) {
+                displayNews(results)
+              }
             })
-            .then(function(){
-                $(window).trigger('hashchange');
+            .then(function () {
+              $(window).trigger('hashchange')
             })
-            //handle errors
-            .catch(function(err){
-                console.log(err);
-            });
-    }
+            // handle errors
+            .catch(function (err) {
+              console.log(err)
+            })
+  }
 
-    function getAllShows(allShows){
-        var showsArray = [],
-            currentTime = moment().format();
+  function displayNews (data) {
+    var $newsDiv = $('.newsDescriptions')
+    data.forEach(function (news) {
+      news.backgroundImg = 'background-image: url(\'' + news.imageUrl + '\')'
+    })
 
-        allShows.forEach(function(item){
-            var showToDisplay = {};
-                showToDisplay.title = item.name;
-                showToDisplay.date = moment(item.start_time).format('MMMM DD YYYY');
-                showToDisplay.time = moment(item.start_time).format('hh:mm a');
-                showToDisplay.venue = item.place.name;
-                showToDisplay.city = item.place.location.city;
-                showToDisplay.state = item.place.location.state;
-                showsArray.push(showToDisplay);
-        });
+    var newsTemplateScript = $('#news-template').html()
+        // Compile the template
+    var newsTemplate = Handlebars.compile(newsTemplateScript)
+    $newsDiv.append(newsTemplate(data))
+  }
+
+  function getShows () {
+        // ajax request to Facebook API
+    var fbEvents = $.ajax({
+      url: '/api/events',
+      method: 'GET'})
+        // Bluebird promise
+    Promise.resolve(fbEvents)
+            // runs when fbEvents is fullfilled
+            .then(function (results) {
+              if (results) {
+                var allShows = getAllShows(results.data)
+                displayShows(allShows)
+              }
+            })
+            .then(function () {
+              $(window).trigger('hashchange')
+            })
+            // handle errors
+            .catch(function (err) {
+              console.log(err)
+            })
+  }
+
+  function getAllShows (allShows) {
+    var showsArray = [],
+      currentTime = moment().format()
+
+    allShows.forEach(function (item) {
+      var showToDisplay = {}
+
+      if (item.place) {
+          showToDisplay.title = item.name
+          showToDisplay.date = moment(item.start_time).format('MMMM DD YYYY')
+          showToDisplay.time = moment(item.start_time).format('hh:mm a')
+          showToDisplay.venue = item.place.name
+          showToDisplay.city = item.place.location.city
+          showToDisplay.state = item.place.location.state
+          showsArray.push(showToDisplay)
+        }
+    })
 
         // sort by date
-        showsArray.sort(function(a, b){
-          var date1 = moment(a.date, 'MMMM DD YYYY'),
-              date2 = moment(b.date, 'MMMM DD YYYY');
-          if (date1.isAfter(date2)) return -1;
-          else if (date1.isBefore(date2)) return 1;
-          else return 0;
-        });
-        return showsArray;
-    }
+    showsArray.sort(function (a, b) {
+      var date1 = moment(a.date, 'MMMM DD YYYY'),
+          date2 = moment(b.date, 'MMMM DD YYYY')
+      if (date1.isAfter(date2)) return -1
+      else if (date1.isBefore(date2)) return 1
+      else return 0
+    })
+    return showsArray
+  }
 
-    function displayShows(data){
-        var $showsDiv = $('.showDescriptions');
+  function displayShows (data) {
+    var $showsDiv = $('.showDescriptions')
 
-        var showsTemplateScript = $("#shows-template").html();
-        //Compile the template​
-        var showsTemplate = Handlebars.compile (showsTemplateScript);
-        $showsDiv.append (showsTemplate(data));
-
-    }
- });
+    var showsTemplateScript = $('#shows-template').html()
+        // Compile the template
+    var showsTemplate = Handlebars.compile(showsTemplateScript)
+    $showsDiv.append(showsTemplate(data))
+  }
+})
